e50b3e1c0198d5d5975a8998f9b684b803e13c2e
JayFeng <fenghaijie@wochu.cn>
2019-04-15 17:36:25 +0800

    Add Demo

---
+++ CaptureScrpllview/CaptureScrpllview/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CaptureScrpllview
+//
+//  Created by wochu on 2019/4/15.
+//  Copyright © 2019年 Fhj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CaptureScrpllview/CaptureScrpllview/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CaptureScrpllview
+//
+//  Created by wochu on 2019/4/15.
+//  Copyright © 2019年 Fhj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
